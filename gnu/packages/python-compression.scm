0e08d4d4eaaa7a9d8192b03d71a250986b625774
Lars-Dominik Braun <lars@6xq.net>
2021-11-25 13:31:23 +0100

    gnu: python-ppmd-cffi: Remove unused inputs.
    * gnu/packages/python-compression.scm (python-ppmd-cffi)
    [native-inputs]: Drop pep517 and setuptools.

--- gnu/packages/python-compression.scm
+++ gnu/packages/python-compression.scm
@@ -137,9 +137,7 @@ and decompression by implementing Python bindings for the ISA-L library.")
     (propagated-inputs
      (list python-cffi))
     (native-inputs
-     (list python-setuptools
-           python-setuptools-scm
-           python-pep517
+     (list python-setuptools-scm
            python-coverage
            python-pytest
            python-pytest-cov))
