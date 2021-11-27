6d1dc03bd82ab7bd0de125f500f81fed00bd26b4
Lars-Dominik Braun <lars@6xq.net>
2021-11-27 16:50:12 +0100

    gnu: gusb-minimal: Add explicit dependency on setuptools.
    * gnu/packages/gnome.scm (gusb-minimal)[native-inputs]: Add
    python-setuptools.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -178,6 +178,7 @@
   #:use-module (gnu packages protobuf)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-crypto)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-xyz)
@@ -6647,7 +6648,7 @@ DAV, and others.")
                    "-Dvapi=false")
                  '()))))
     (native-inputs
-     (list gobject-introspection pkg-config vala))
+     (list gobject-introspection pkg-config vala python-setuptools))
     (propagated-inputs
      ;; Both of these are required by gusb.pc.
      (list glib libusb))
