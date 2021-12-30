a9eab891b7707a1462a2a0fc2777835b9879b1a4
Lars-Dominik Braun <lars@6xq.net>
2021-12-30 09:48:09 +0100

    gnu: python-pytest-qt: Remove obsolete test target.
    * gnu/packages/python-check.scm (python-pytest-qt)[arguments]: Remove #:test-target.

--- gnu/packages/python-check.scm
+++ gnu/packages/python-check.scm
@@ -1311,8 +1311,7 @@ libraries.")
         (base32 "09c9psfn3zigpaw1l1cmynpa3csxa49wc2ih5lzl24skdkw0njvi"))))
     (build-system python-build-system)
     (arguments
-     `(#:test-target "pytest"
-       #:phases
+     `(#:phases
        (modify-phases %standard-phases
          (add-before 'check 'set-qpa
            (lambda _
