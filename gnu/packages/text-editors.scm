fe799822b8c53ba24f06e9f2882f85c3c87864b1
Lars-Dominik Braun <lars@6xq.net>
2022-01-05 19:37:25 +0100

    gnu: Remove remaining uses of #:use-setuptools?.
    * gnu/packages/text-editors.scm (virtaal)[arguments]: Remove #:use-setuptools.
    * gnu/packages/virtualization.scm (virt-manager): Ditto.
    * gnu/packages/wicd.scm (wicd): Ditto.
    * gnu/packages/wxwidgets.scm (python2-wxpython): Ditto.

--- gnu/packages/text-editors.scm
+++ gnu/packages/text-editors.scm
@@ -1143,7 +1143,6 @@ similar to vi/ex.")
     (build-system python-build-system)
     (arguments
      `(#:python ,python-2
-       #:use-setuptools? #f
        #:tests? #f ;; Failing tests
        #:phases
        (modify-phases %standard-phases
